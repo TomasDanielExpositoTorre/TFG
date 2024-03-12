fe6154d77225a621ebc9bfe98d8f6d3c615138f7
TomasDanielExpositoTorre <tomas.exposito@estudiante.uam.es>
2024-03-12 15:35:15 +0000

    feat(kernel): base algorithm structure

--- gpuhostnic/src/kernel.cu
+++ gpuhostnic/src/kernel.cu
@@ -1,95 +1,115 @@
 #include "headers.h"
 
-/**
- * About capping...
- * Who should do it, the kernel (VERY divergent) or the transmission core?
- * Can we tell TX up to how many bytes we want to write?
- *  e.g. : 0 for not yet read, header_len for cap, -1 for whole packet on extmem
- *
- *
- * About filtering...
- * Who should handle it, the GPU or CPU?
- * Can we write a dpdk filter beforehand?
- */
+#define MIN_ASCII 0x20
+#define MAX_ASCII 0x7E
 
-__global__ void vanilla_capping_thread(struct rte_gpu_comm_list * comm_list, kernel_args args)
+__global__ void vanilla_capping_thread(struct rte_gpu_comm_list *comm_list, kernel_args args)
 {
-    printf("Hello Kernel!\n");
     /**
-     * TODO
-     *
-     * for each packet in burst do
-     *  i = header_len(packet)
-     *  if i is unsupported do not write packet
-     *  for byte in payload do
-     *      if byte[i] is printable run++ total++
-     *          if run == runlen DO NOT CAP
-     *      else run = 0
-     *
-     *  if total >= percentage DO NOT CAP
-     *  else CAP
+     * Caculate header len,
+     * Or assume ~80 bytes,
+     * Or use a magic dpdk function to do it for me
      */
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int consecutive = 0, total = 0, headerlen = 0;
+    char *packet;
+
+    if (i < comm_list->num_pkts)
+    {
+        packet = (char *)(comm_list->pkt_list[i].addr);
+        for (int j = headerlen; j < comm_list->pkt_list[i].size; j++)
+        {
+            if (packet[j] >= MIN_ASCII && packet[j] <= MAX_ASCII)
+            {
+                consecutive++;
+                total += 100;
+                if (consecutive == args.ascii_runlen)
+                    return; /* Do not cap */
+            }
+            else
+                consecutive = 0;
+        }
+
+        if (total >= (args.ascii_percentage * comm_list->pkt_list[i].size))
+            return; /* Do not cap */
+
+        comm_list->pkt_list[i].size = headerlen; /* Cap when writing to CPU */
+    }
 }
 
-__global__ void optimized_capping_thread(struct rte_gpu_comm_list * comm_list, kernel_args args)
+__global__ void optimized_capping_thread(struct rte_gpu_comm_list *comm_list, kernel_args args)
 {
-    printf("Hello Kernel!\n");
     /**
-     * TODO
-     *
-     * for each packet in burst do
-     *  i = header_len(packet)
-     *  if i is unsupported do not write packet
-     *  for byte in payload do
-     *      if byte[i] is printable run++ total++
-     *          if run == runlen DO NOT CAP
-     *      else run = 0, i += runlen
-     *
-     *  if total >= percentage DO NOT CAP
-     *  else CAP
+     * Caculate header len,
+     * Or assume ~80 bytes,
+     * Or use a magic dpdk function to do it for me
      */
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    int consecutive = 0, total = 0, headerlen = 0, seen = 0;
+    char *packet;
+
+    if (i < comm_list->num_pkts)
+    {
+        /* Cap if payload is less than runlen */
+        if (headerlen + args.ascii_runlen - 1 > comm_list->pkt_list[i].size)
+        {
+            comm_list->pkt_list[i].size = headerlen;
+            return;
+        }
+
+        packet = (char *)(comm_list->pkt_list[i].addr);
+        for (int j = headerlen + args.ascii_runlen - 1; j >= headerlen && j < comm_list->pkt_list[i].size; j--, seen++)
+        {
+            if (packet[j] >= MIN_ASCII && packet[j] <= MAX_ASCII)
+            {
+                consecutive++;
+                total += 100;
+                if (consecutive == args.ascii_runlen)
+                    return; /* Do not cap */
+            }
+            else
+            {
+                consecutive = 0;
+                j += args.ascii_runlen + 1;
+            }
+        }
+
+        if (total >= (args.ascii_percentage * seen))
+            return; /* Do not cap */
+
+        comm_list->pkt_list[i].size = headerlen; /* Cap when writing to CPU */
+    }
 }
 
-__global__ void naive_capping_warp(struct rte_gpu_comm_list * comm_list, kernel_args args)
+__global__ void naive_capping_warp(struct rte_gpu_comm_list *comm_list, kernel_args args)
 {
-    printf("Hello Kernel!\n");
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    printf("%d\n", i);
 }
 
-__global__ void invasive_capping_warp(struct rte_gpu_comm_list * comm_list, kernel_args args)
+__global__ void invasive_capping_warp(struct rte_gpu_comm_list *comm_list, kernel_args args)
 {
-    printf("Hello Kernel!\n");
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    printf("%d\n", i);
 }
 
-__global__ void coercive_capping_warp(struct rte_gpu_comm_list * comm_list, kernel_args args)
+__global__ void coercive_capping_warp(struct rte_gpu_comm_list *comm_list, kernel_args args)
 {
-    printf("Hello Kernel!\n");
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+    printf("%d\n", i);
 }
 
-void launch_kernel(struct rte_gpu_comm_list * comm_list, int blocks, int threads, cudaStream_t stream, kernel_args args)
+void launch_kernel(struct rte_gpu_comm_list *comm_list, int blocks, int threads, cudaStream_t stream, kernel_args args)
 {
-    
+
     if (args.kernel == VANILLA_CAPPING_THREAD)
-    {
         vanilla_capping_thread<<<blocks, threads, 0, stream>>>(comm_list, args);
-    }
     else if (args.kernel == OPTIMIZED_CAPPING_THREAD)
-    {
         optimized_capping_thread<<<blocks, threads, 0, stream>>>(comm_list, args);
-        // opt_pkthread_kernel<<<1,1>>>();
-    }
     else if (args.kernel == NAIVE_CAPPING_WARP)
-    {
         naive_capping_warp<<<blocks, threads, 0, stream>>>(comm_list, args);
-        // naive_pktwarp_kernel<<<1,1>>>();
-    }
     else if (args.kernel == INVASIVE_CAPPING_WARP)
-    {
         invasive_capping_warp<<<blocks, threads, 0, stream>>>(comm_list, args);
-        // invasive_pktwarp_kernel<<<1,1>>>();
-    }
     else
-    {
         coercive_capping_warp<<<blocks, threads, 0, stream>>>(comm_list, args);
-        // coercive_pktwarp_kernel<<<1,1>>>();
-    }
 }
