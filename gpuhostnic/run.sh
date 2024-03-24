e087ec1627c0b3b64de2e3fd7670bb37d91c5741
TomasDanielExpositoTorre <tomas.exposito@estudiante.uam.es>
2024-03-24 16:45:49 +0000

    refactor(script): compile, default and custom arguments for gpu-hostnic

--- gpuhostnic/run.sh
+++ gpuhostnic/run.sh
@@ -1,10 +1,14 @@
-if [ "$1" == "c" ]
+if [ "$1" == "--compile" ]
 then
     echo "Loading environment variables"
     source env.sh
     echo "Recompiling..."
     ninja -C build
-else
+elif [ "$1" == "--default" ]
+then
     echo "Running program..."
-    sudo bash -c 'source env.sh && ./build/gpu-hostnic -a 33:00.0 -a 36:00.0 -l 0-4 -- -o "test.pcap"'
+    sudo bash -c 'source env.sh && ./build/gpu-hostnic -a 33:00.0 -a 36:00.0 -l 0-4 -- -o "test.pcap" -q 2'
+else
+    args="source env.sh && ./build/gpu-hostnic -a 33:00.0 -a 36:00.0 -l 0-4 -- ${@:1}"
+    sudo bash -c "$args"
 fi
